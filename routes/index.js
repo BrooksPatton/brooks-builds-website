6de6b6489ee3085e8e92292bb94982f23b6e7218
Brooks Patton <scgryphon@sbcglobal.net>
2017-06-18 15:08:29 -0600

    created a logo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const isStreaming = false;
+
+  res.render('index', { streaming: isStreaming });
 });
 
 module.exports = router;
