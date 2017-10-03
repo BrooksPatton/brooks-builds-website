1cde0827ddf11ad7afc9c06a4398d04ad7db98c7
Brooks Patton <brooks@brookzerker.com>
2017-10-02 21:19:34 -0600

    current twitch status can now be seen

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './Navbar.css';
+import TwitchStatus from './TwitchStatus';
 
 class Navbar extends Component {
   render() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <a className="navbar-brand" href="/">BB</a>
+
+          <div className="ml-auto">
+            <TwitchStatus />
+          </div>
         </nav>
         );
   }
